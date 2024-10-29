234bdb492d8c800758fde207403d32fbb8c67072
BrianTzr <briantazar@gmail.com>
2024-10-29 19:54:15 +0700

    Finished Code

--- app/src/main/java/com/example/uts/DetailActivity.kt
+++ app/src/main/java/com/example/uts/DetailActivity.kt
@@ -1,13 +1,15 @@
 package com.example.uts
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.text.Html
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
-class DetailActivity: AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     private lateinit var card: CardData
 
@@ -15,22 +17,35 @@ class DetailActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        // Retrieve the card data from the intent
         card = intent.getSerializableExtra("card") as CardData
 
         val cardImage = findViewById<ImageView>(R.id.cardImage)
         val cardName = findViewById<TextView>(R.id.cardName)
         val cardDesc = findViewById<TextView>(R.id.cardDesc)
-//        val cardType = findViewById<TextView>(R.id.cardType)
-//        val cardRace = findViewById<TextView>(R.id.cardRace)
-//        val cardLink = findViewById<TextView>(R.id.cardLink)
+        val cardType = findViewById<TextView>(R.id.cardType)
+        val cardRace = findViewById<TextView>(R.id.cardRace)
+        val cardArchetype = findViewById<TextView>(R.id.cardArchetype)
+        val cardLink = findViewById<TextView>(R.id.cardLink)
 
+        // Load the image using Glide
         Glide.with(cardImage.context)
             .load(card.card_images[0].image_url_cropped)
             .into(cardImage)
-        cardName.text = card.name
+
+        // Set the TextViews with card data
+        cardName.text = Html.fromHtml("<u>${card.name}</u>")
         cardDesc.text = card.desc
-//        cardType.text = card.type
-//        cardRace.text = card.race
-//        cardLink.text = card.ygoprodeck_url
+        cardType.text = "Card Type: ${card.type} / ${card.humanReadableCardType}"
+        cardRace.text = "Race     : ${card.race}"
+        cardArchetype.text = "Archetype: ${card.archetype}"
+
+        // Set the link text and make it clickable
+        cardLink.text = card.ygoprodeck_url
+        cardLink.setOnClickListener {
+            val uri = Uri.parse(card.ygoprodeck_url)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
